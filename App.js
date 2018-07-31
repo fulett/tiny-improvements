d19f82c3e6bf9c34e03ed08cfe8b1bb5c0332ca2
fulett <41027008+fulett@users.noreply.github.com>
2018-07-30 23:44:02 -0400

    Add files via upload
    Files associated with Homework 2 as having issues w/ rendering + committing to Git.

---
+++ App.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import { Col, Container, Row, Form, FormGroup, Input, Label, Button, Card, CardBody } from "reactstrap";
+import AwardCard from './components/AwardCard';
+import axios from "axios";
+import KudosForm from './components/KudosForm';
+
+class App extends Component {
+  state = {
+    users: [],
+    awards: [],
+    kudosText: "",
+    kudosTitle: "",
+    sender: "",
+    receiver: ""
+  }
+
+  updateKudosText = event => {
+    this.setState({ kudosText: event.target.value });
+  }
+
+  updateKudosTitle = event => {
+    this.setState({ kudosTitle: event.target.value });
+  }
+
+  updateReceiver = event => {
+    this.setState({ receiver: event.target.value });
+  }
+
+  updateSender = event => {
+    this.setState({ sender: event.target.value });
+  }
+
+
+
+  postData = () => {
+    axios.post("/api/kudos", {
+      text: this.state.kudosText,
+      title: this.state.kudosTitle,
+      sender: this.state.sender,
+      receiver: this.state.receiver,
+    })
+      .then(response => {
+        this.setState({
+          awards: response.data
+        })
+      })
+  }
+
+  componentDidMount = () => {
+    axios.get("/api/kudos")
+      .then(response => {
+        this.setState({
+          awards: response.data
+        })
+      })
+
+    axios.get("/api/users")
+      .then(response => {
+        this.setState({
+          users: response.data
+        })
+      })
+  }
+
+
+  render() {
+    return (
+      <Container>
+        <Row>
+          <Col md="12">
+            <h1>Tiny Progress</h1>
+          </Col>
+        </Row>
+        <br />
+        <Row>
+          <Col md="12" lg="9">
+            {this.state.awards.map(award => <AwardCard title={award.title} comment={award.comment} receiver={award.receiver} sender={award.sender} text={award.text} />)}
+          </Col>
+        </Row>
+
+        <Row>
+          <Col md="12">
+            <KudosForm
+              users={this.state.users}
+              postData={this.postData}
+              kudosText={this.state.kudosText}
+              kudosTitle={this.state.kudosTitle}
+              updateKudosText={this.updateKudosText}
+              updateKudosTitle={this.updateKudosTitle}
+              updateReceiver={this.updateReceiver}
+              updateSender={this.updateSender}
+              receiver={this.state.users.map(user => <option> {user.name} </option>)}
+              sender={this.state.users.map(user => <option> {user.name} </option>)}
+            />
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
+}
---
+++ KudosForm.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Form, FormGroup, Label, Input, Button } from "reactstrap";
+
+const KudosForm = props => (
+  <Form>
+    <FormGroup>
+      <Label>Give Kudos to</Label>
+      <Input type="select"
+        onChange={props.updateReceiver}>
+        value={props.receiver}
+      </Input>
+    </FormGroup>
+    <FormGroup>
+      <Input type="text" placeholder="Kudos Title"
+        onChange={props.updateKudosTitle}
+        value={props.kudosTitle}
+      />
+    </FormGroup>
+    <FormGroup>
+      <Input type="text" placeholder="Kudos Text"
+        onChange={props.updateKudosText}
+        value={props.kudosText}
+      />
+    </FormGroup>
+    
+    <FormGroup>
+      <Button onClick={props.postData}> Give Kudos </Button>
+    </FormGroup>
+  </Form >
+)
+
+export default KudosForm;
